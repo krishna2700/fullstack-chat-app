bf2630f8ccf41d4fec48f5e61a98783c704088a8
krishna2700 <krishnaruparelia0207@gmail.com>
2025-11-12 12:08:30 +0530

    backend se up and fe react installation

---
+++ backend/src/index.js
@@ -0,0 +1,14 @@
+import express from "express";
+import dotenv from "dotenv";
+import authRoutes from "./routes/auth.routes.js";
+
+dotenv.config();
+const app = express();
+
+const PORT = process.env.PORT;
+
+app.use("/api/auth", authRoutes);
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
