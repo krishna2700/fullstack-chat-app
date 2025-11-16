140154a561d5d90f9a82655a79f3cec14b9d0588
krishna2700 <krishnaruparelia0207@gmail.com>
2025-11-16 14:02:39 +0530

    socket io implementation

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,30 +1,39 @@
-import cookieParser from "cookie-parser";
-import dotenv from "dotenv";
 import express from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import { app, server } from "./lib/socket.js";
+import { connectDB } from "./lib/db.js";
 import authRoutes from "./routes/auth.routes.js";
 import messageRoutes from "./routes/message.routes.js";
-import cors from "cors";
-
-import { connectDB } from "./lib/db.js";
+import path from "path";
 
 dotenv.config();
-const app = express();
 
 const PORT = process.env.PORT;
+const __dirname = path.resolve();
 
 app.use(express.json());
 app.use(cookieParser());
-
 app.use(
   cors({
     origin: "http://localhost:5173",
     credentials: true,
   })
 );
+
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
-app.listen(PORT, () => {
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
+
+server.listen(PORT, () => {
   console.log("server is running on PORT:" + PORT);
   connectDB();
 });
