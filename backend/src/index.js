60fe739ef2e51459f037850788bf8ce8b858c7ee
krishna2700 <krishnaruparelia0207@gmail.com>
2025-11-17 11:13:43 +0530

    debug fix for build

--- backend/src/index.js
+++ backend/src/index.js
@@ -14,7 +14,6 @@ const __dirname = path.resolve();
 
 app.use(express.json());
 app.use(cookieParser());
-
 app.use(
   cors({
     origin: process.env.CLIENT_URL || "http://localhost:5173",
@@ -28,12 +27,11 @@ app.use("/api/messages", messageRoutes);
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-  app.get("/*", (req, res) => {
+  app.get(/.*/, (req, res) => {
     res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
   });
 }
 
 server.listen(PORT, () => {
-  console.log(`Server running on port: ${PORT}`);
   connectDB();
 });
